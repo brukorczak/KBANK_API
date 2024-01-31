0f4c077ab497d7993d2e3f4cb8deb2b828a574fb
Bruna Diele Korczak Trino <bkorczak45@gmail.com>
2024-01-31 13:40:10 -0300

    db.sql

--- db/db.sql
+++ db/db.sql
@@ -1,8 +1,8 @@
 -- Criar o banco de dados
-CREATE DATABASE IF NOT EXISTS banco;
+CREATE DATABASE IF NOT EXISTS bank;
 
 -- Selecionar o banco de dados
-USE banco;
+USE bank;
 
 -- Criar a tabela de usuários
 CREATE TABLE users (
@@ -10,41 +10,23 @@ CREATE TABLE users (
     name varchar(100) not null,
     age integer not null,
     phone varchar(20) not null,
-    address varchar(60) not null
+    address varchar(60) not null,
+    cpf varchar(15) not null,
+    password varchar(15) not null
 );
 
 -- Criar a tabela de contas
 CREATE TABLE accounts (
     id bigint not null AUTO_INCREMENT primary key,
-    account_number varchar(20) not null unique,
+    accountNumber varchar(20) not null unique,
     accountType varchar(20) not null,
     balance double not null,
     userId bigint not null,
     foreign key (userId) references users(id)
 );
 
--- Inserir dados na tabela 'users'
-INSERT INTO users (name, age, phone, address, accountType) VALUES ('test1', 19, '19991144324', '12345-678 Cityville, 123 Street', CURRENT);
 
--- Selecionar todos os registros da tabela 'accounts' onde 'accountType' é 'CURRENT'
-SELECT * FROM accounts WHERE accountType = 'CURRENT';
--- Selecionar todos os registros da tabela 'accounts' onde 'accountType' é 'SAVINGS'
-SELECT * FROM accounts WHERE accountType = 'SAVINGS';
-
--- Selecionar todos os registros da tabela 'accounts'
+-- Selecionar todos os registros das tabelas
+SELECT * FROM users;
 SELECT * FROM accounts;
 
--- Excluir todos os registros da tabela 'accounts'
-DELETE FROM accounts;
-
--- Desativar o modo de atualização segura
-SET SQL_SAFE_UPDATES = 0;
-
--- Excluir todos os registros da tabela 'users'
-DELETE FROM users;
-
--- Excluir todos os registros da tabela 'accounts'
-DELETE FROM accounts;
-
--- Ativar o modo de atualização segura
-SET SQL_SAFE_UPDATES = 1;
